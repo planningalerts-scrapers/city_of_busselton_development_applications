6ea7b550471a8de37e6de229f01bb290dbc0cffc
Ross Paine <ashtermet@gmail.com>
2016-09-10 20:26:20 +0800

    slightly different approach to handling 'malformed' titles

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,9 @@ require 'mechanize'
 require 'uri'
 require 'date'
 
+class BadData < Exception
+end
+
 agent = Mechanize.new
 
 # Read in a page
@@ -20,27 +23,33 @@ list.each do |row|
 
   title = row.search('h2.list-item-title').inner_html.strip
 
-  if title =~ /^DA(\d{2})\/(\d{4}) -? (.*) -?\s*Lot (\d+) (.*)/
-  #               year    int      description   lot-number rest-of-address
+  begin
     record = {}
-    record['council_reference'] = "DA#{$1}/#{$2}"
-    record['address']           = "Lot #{$4} #{$5}, WA"
-    record['description']       = $3.gsub(/ -$/, '')
-    record['info_url']          = row.search('a').attr("href").to_s
-    record['comment_url']       = "mailto:city@busselton.wa.gov.au?subject=#{record['council_reference']}"
-    record['date_scraped']      = Date.today.to_s
+    if title !~ /^DA/
+      raise BadData, "Skipping #{title} because it is not a DA"
+    end
 
-    on_notice_to_text           = row.search('p.applications-closing').inner_html.match(/Submissions closing on (.*)/)[1]
-    record['on_notice_to']      = Date.parse(on_notice_to_text).iso8601
+    if title =~ /^DA(\d{2})\/(\d{4}) -? (.*) -?\s*Lot (\d+) (.*)/
+      #               year    int      description   lot-number rest-of-address
+      record['council_reference'] = "DA#{$1}/#{$2}"
+      record['address']           = "Lot #{$4} #{$5}, WA"
+      record['description']       = $3.gsub(/ -$/, '')
+      record['info_url']          = row.search('a').attr("href").to_s
+      record['comment_url']       = "mailto:city@busselton.wa.gov.au?subject=#{record['council_reference']}"
+      record['date_scraped']      = Date.today.to_s
+
+      on_notice_to_text           = row.search('p.applications-closing').inner_html.match(/Submissions closing on (.*)/)[1]
+      record['on_notice_to']      = Date.parse(on_notice_to_text).iso8601
+     end
+
+    raise BadData, "Failed to match #{title} to any expression" if record.empty?
 
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
       ScraperWiki.save_sqlite(['council_reference'], record)
     else
       puts "Skipping already saved record " + record['council_reference']
     end
-  else
-    # if it doesn't match the regexp above it's probably not a DA
-    # hopefully it's not just a strange string form..
-    $stderr.puts "Skipping #{title.strip.chomp.strip.chomp} as it is not a DA"
+  rescue BadData
+    $stderr.puts $!
   end
 end
