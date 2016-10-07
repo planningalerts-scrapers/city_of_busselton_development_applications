85824a1f40ddb7b394f872ddcc49d82368259db6
Ross Paine <ashtermet@gmail.com>
2016-10-07 19:53:40 +0800

    deeper scraping
    A recent DA was over two lots, listing only the lot numbers and not the
    HSE in the listing page. This requires deeper scraping, fetching the
    info page for the DA and looking through the very non-semantic text.

--- scraper.rb
+++ scraper.rb
@@ -11,7 +11,9 @@ end
 agent = Mechanize.new
 
 # Read in a page
-WEBSITE_URI=URI('http://www.busselton.wa.gov.au/Developing-Busselton/Public-Consultation')
+WEBSITE_URI=URI('http://www.busselton.wa.gov.au')
+WEBSITE_URI.path = '/Developing-Busselton/Public-Consultation'
+
 page = agent.get(WEBSITE_URI.to_s)
 
 # get the list of development applications
@@ -24,6 +26,7 @@ list.each do |row|
   title = row.search('h2.list-item-title').inner_html.strip
 
   begin
+    puts "Attempting '#{title}'"
     record = {}
     if title !~ /^DA/
       raise BadData, "Skipping #{title} because it is not a DA"
@@ -41,7 +44,44 @@ list.each do |row|
 
       on_notice_to_text           = row.search('p.applications-closing').inner_html.match(/Submissions closing on (.*)/)[1]
       record['on_notice_to']      = Date.parse(on_notice_to_text).iso8601
-     end
+    end
+
+    if record.empty?
+      # maybe the information can be obtained from the info page
+      inner_page = agent.get(row.search('a').attr('href').to_s)
+ 
+      title = inner_page.search('head > title')
+      if title.inner_html =~ /DA(\d{2})\/(\d{4})/
+        record['council_reference'] = "DA#{$1}/#{$2}"
+      else
+        raise BadData, "Failed to find council reference number in second level page"
+      end
+
+      inner_page.search('strong').each do |element|
+        element.text.gsub!('&nbsp;', ' ')
+
+        if element.text.include?('PROPERTY:')
+          if element.text =~ /Lots? (.+) \(Hse (?:NO)? ?(\d+)\) (.*)/i
+            record['address'] = "#{$2} #{$3}, WA"
+            record['lot_number'] = $1
+          end
+        elsif element.text =~ /PROPOSED DEVELOPMENT:\s*(.*)/
+          record['description'] = $1.strip
+        end
+
+        begin
+          record['on_notice_to'] = Date.parse(element.text).iso8601
+        rescue ArgumentError; end
+      end
+
+      ['address', 'lot_number', 'description', 'on_notice_to'].each do |f|
+        raise BadData, "Could not find #{f.gsub('_', ' ')} in second level page." unless record[f]
+      end
+
+      record['info_url']     = row.search('a').attr('href').to_s or raise BadData, "Could not find info url"
+      record['comment_url']  = "mailto:city@busselton.wa.gov.au?subject=#{record['council_reference']}"
+      record['date_scraped'] = Date.today.to_s
+    end
 
     raise BadData, "Failed to match #{title} to any expression" if record.empty?
 
@@ -51,6 +91,8 @@ list.each do |row|
       puts "Skipping already saved record " + record['council_reference']
     end
   rescue BadData
-    $stderr.puts $!
+    # selector #main-content > div > div:nth-child(1) > p:nth-child(8) > strong:nth-child(3)
+    # head > title
+    $stderr.puts "\e[31m\e[1m#{$!}\e[0m"
   end
 end
