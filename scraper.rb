f2457a752157138f43de204917f060df4b91e409
Ross Paine <ashtermet@gmail.com>
2016-10-19 21:28:39 +0800

    rewrite of scraper
    The City of Busselton have implemented a clean XML feed of DAs that
    simplifies the scraping a great deal. It should also reduce any errors,
    particularly around DAs that cover more than one lot.

--- scraper.rb
+++ scraper.rb
@@ -1,55 +1,37 @@
 #!/usr/bin/env ruby
 
 require 'scraperwiki'
-require 'mechanize'
 require 'uri'
-require 'date'
+require 'rexml/document'
+require 'logger'
 
-class BadData < Exception
-end
+WEBSITE_URI=URI('http://www.busselton.wa.gov.au/externaldata/CoB_Advertised_DAs.xml')
 
-agent = Mechanize.new
+TEMPLATE_RECORD = {
+  'council_reference' => nil,
+  'address' => nil,
+  'description' => nil,
+  'info_url' => nil,
+  'comment_url' => nil,
+  'date_received' => nil,
+  'on_notice_from' => nil,
+  'on_notice_to' => nil,
+}
 
-# Read in a page
-WEBSITE_URI=URI('http://www.busselton.wa.gov.au/Developing-Busselton/Public-Consultation')
-page = agent.get(WEBSITE_URI.to_s)
+@logger = Logger.new(STDOUT)
 
-# get the list of development applications
-list = page.search('div.list-item-container')
+@document = REXML::Document.new(open(WEBSITE_URI.to_s))
 
-list.each do |row|
-  # most of the required information is contained in one string
-  # inside the h2 element h2.list-item-title
+@document.elements.each('/planning/applications/application') do |application|
+  record = TEMPLATE_RECORD.clone
+  record.each do |key, value|
+    record[key] = application.elements[key].text
+  end
 
-  title = row.search('h2.list-item-title').inner_html.strip
-
-  begin
-    record = {}
-    if title !~ /^DA/
-      raise BadData, "Skipping #{title} because it is not a DA"
-    end
-
-    if title =~ /^DA(\d{2})\/(\d{4}) -? (.*) -?\s*Lot (\d+) \(Hse (\d+)\) (.*)/
-      #               year    int      description   lot-number rest-of-address
-      record['council_reference'] = "DA#{$1}/#{$2}"
-      record['address']           = "#{$5} #{$6}, WA"
-      record['description']       = $3.gsub(/ -$/, '')
-      record['info_url']          = row.search('a').attr("href").to_s
-      record['comment_url']       = "mailto:city@busselton.wa.gov.au?subject=#{record['council_reference']}"
-      record['date_scraped']      = Date.today.to_s
-
-      on_notice_to_text           = row.search('p.applications-closing').inner_html.match(/Submissions closing on (.*)/)[1]
-      record['on_notice_to']      = Date.parse(on_notice_to_text).iso8601
-     end
-
-    raise BadData, "Failed to match #{title} to any expression" if record.empty?
-
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      ScraperWiki.save_sqlite(['council_reference'], record)
-    else
-      puts "Skipping already saved record " + record['council_reference']
-    end
-  rescue BadData
-    $stderr.puts $!
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+    @logger.info "Saving new record #{record['council_reference']} #{record['description']}"
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  else
+    @logger.info "Skipping already saved record " + record['council_reference']
   end
 end
