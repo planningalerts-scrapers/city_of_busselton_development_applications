dba1b8f5690c29590018df236ecfeb88da922585
Ross Paine <ashtermet@gmail.com>
2016-10-20 16:20:59 +0800

    hybrid approach to finding the relevent info..
    Since it's not possible to determine at the database level what the
    title of the advertising URL will be, this commit takes a hybrid
    approach.
    
    The idea is to read the provided XML document, then use the
    council_reference value to search the list of advertising documents for
    the relevent url.

--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,95 @@
 #!/usr/bin/env ruby
 
 require 'scraperwiki'
+require 'mechanize'
 require 'uri'
 require 'rexml/document'
 require 'logger'
 
-DATA_URI=URI('http://www.busselton.wa.gov.au/externaldata/CoB_Advertised_DAs.xml')
-WEBSITE_URI=URI('http://www.busselton.wa.gov.au/Developing-Busselton/Public-Consultation')
+class CoBPlanningScraper
 
-TEMPLATE_RECORD = {
-  'council_reference' => nil,
-  'address' => nil,
-  'description' => nil,
-  'info_url' => nil,
-  'comment_url' => nil,
-  'date_received' => nil,
-  'on_notice_from' => nil,
-  'on_notice_to' => nil,
-}
+  # Error to raise when you can't find stuff.
+  class BadData < Exception; end
 
-@logger = Logger.new(STDOUT)
+  # URI of the generated XML document containing the advertised proposals for
+  # comment
+  DATA_URI=URI('http://www.busselton.wa.gov.au/externaldata/CoB_Advertised_DAs.xml')
 
-@document = REXML::Document.new(open(DATA_URI.to_s))
+  # The advertising document for public consultations
+  WEBSITE_URI=URI('http://www.busselton.wa.gov.au/Developing-Busselton/Public-Consultation')
 
-@document.elements.each('/planning/applications/application') do |application|
-  record = TEMPLATE_RECORD.clone
-  record.each do |key, value|
-    record[key] = application.elements[key].text
+  # A template of the object that is ultimately recorded into the database.
+  TEMPLATE_RECORD = {
+    'council_reference' => nil,
+    'address' => nil,
+    'description' => nil,
+    'info_url' => nil,
+    'comment_url' => nil,
+    'date_received' => nil,
+    'on_notice_from' => nil,
+    'on_notice_to' => nil,
+  }
+
+  def initialize()
+    @agent = Mechanize.new
+    @logger = Logger.new(STDOUT)
+
+    @document = REXML::Document.new(open(DATA_URI.to_s))
+
+    @page = @agent.get(WEBSITE_URI.to_s)
+
+    # get the list of development applications
+    @list = @page.search('div.list-item-container')
+
   end
 
-  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-    @logger.info "Saving new record #{record['council_reference']} #{record['description']}"
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  else
-    @logger.info "Skipping already saved record " + record['council_reference']
+  def find_info_url(council_reference)
+    @list.each do |row|
+      title = row.search('h2.list-item-title').inner_html.strip
+      href = row.search('a').attr('href').to_s
+
+      if title =~ /#{council_reference}/
+        return href
+      end
+
+      if council_reference =~ /^AMD(\d{2})\/(\d{4})/
+	scheme_number = $1.to_i
+	amendment_number = $2.to_i
+
+	if title =~ /local planning scheme no. ?#{scheme_number}.* amendment no. ?#{amendment_number}/i
+          return href
+	end
+      end
+    end
+
+    raise BadData, "Could not find the page URL for #{council_reference}"
+  end
+
+
+  def process
+    @document.elements.each('/planning/applications/application') do |application|
+      record = TEMPLATE_RECORD.clone
+
+      record.each do |key, value|
+	begin
+          record[key] = application.elements[key].text
+	rescue NoMethodError
+          record[key] = "No #{key} found in XML record"
+	end
+      end
+
+      # override XML supplied values with scraped values
+      record['info_url'] = find_info_url(record['council_reference'])
+      record['comment_url'] = record['info_url']
+
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        @logger.info "Saving new record #{record['council_reference']} #{record['description']}"
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        @logger.info "Skipping already saved record " + record['council_reference']
+      end
+    end
   end
 end
+
+CoBPlanningScraper.new.process
